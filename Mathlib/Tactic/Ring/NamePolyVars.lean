4992543a4f11bf39b2f436961efdb9e44f7c85ab
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-08-15 08:38:02 +0100

    slightly unhygienic formulation

--- Mathlib/Tactic/Ring/NamePolyVars.lean
+++ Mathlib/Tactic/Ring/NamePolyVars.lean
@@ -1,15 +1,19 @@
 /-
 Copyright (c) 2025 Adam Topaz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Topaz
+Authors: Adam Topaz, Kenny Lau
 -/
-import Mathlib.Algebra.MvPolynomial.Basic
-import Mathlib.Algebra.Polynomial.Basic
+
+import Lean
+
+-- import Mathlib.Algebra.MvPolynomial.Basic
+-- import Mathlib.Algebra.Polynomial.Basic
 
 /-!
-The command `name_poly_vars` names variables in
-`MvPolynomial (Fin n) R` for the appropriate value of `n`,
-or `Polynomial (Polynomial (... R))` stacked appropriately.
+The command `name_poly_vars` names variables in any combination of `Polynomial`, `MvPolynomial`,
+`RatFunc`, `PowerSeries`, `MvPowerSeries`, and `LaurentSeries`, where the `Mv` is restricted to
+`Fin n`.
+
 The notation introduced by this command is local.
 
 Usage:
@@ -17,103 +21,293 @@ Usage:
 ```lean
 variable (R : Type) [CommRing R]
 
-name_poly_vars R[X,Y,Z]
-name_poly_vars R[s][t][u]
+name_poly_vars R[[X,Y]][t]⸨a⸩(u)
 
-#check Y -- Y : MvPolynomial (Fin 3) R
-#check t -- t : Polynomial (Polynomial (Polynomial R))
+#check X -- X : R[[X,Y]][t]⸨a⸩(u)
+#check t -- t : R[[X,Y]][t]⸨a⸩(u)
 ```
+
+For the edge case of `MvPolynomial (Fin 1) R`, use the syntax `R[u,]` with a trailing comma.
 -/
 
 open Lean Elab Command
 
-namespace Mathlib.Tactic
+initialize registerTraceClass `name_poly_vars
+
+namespace Mathlib.Tactic.NamePolyVars
+
+section Syntax
 
 /--
 A variable that can be in the head position of a `name_poly_vars` command.
 This is either an identifier or a term enclosed in parentheses.
 -/
-syntax polyVarsHead := ident <|> ("(" term ")")
+syntax polyesqueHead := ident <|> ("(" term ")")
 
-/--
-Convert a `polyVarsHead` to a term.
--/
-def polyVarsHeadToTerm? (stx : TSyntax ``polyVarsHead) : Option Term :=
-  match stx with
-  | `(polyVarsHead| $k:ident) => some { raw := k.raw }
-  | `(polyVarsHead| ($u:term)) => some u
+-- syntax to allow specifying `MvPolynomial (Fin 1) R` with `R[t,]` and `Polynomial R` with `R[t]`
+syntax polyesqueMv? := sepBy(ident,",",",",allowTrailingSep)
+
+syntax polynomialMv? := "[" polyesqueMv? "]"
+syntax powerSeriesMv? := "[[" polyesqueMv? "]]"
+syntax ratFunc := "(" ident ")"
+syntax laurentSeries := "⸨" ident "⸩"
+
+syntax polyesqueBody := polynomialMv? <|> powerSeriesMv? <|> ratFunc <|> laurentSeries
+syntax polyesque := polyesqueHead noWs polyesqueBody+
+
+syntax (name := polyesque') polyesque : term
+
+abbrev PolyesqueHead : Type := TSyntax ``polyesqueHead
+
+abbrev Polyesque : Type := TSyntax ``polyesque
+
+/-- Convert a `polyesqueHead` to a term. -/
+def PolyesqueHead.term? : PolyesqueHead → Option Term
+  | `(polyesqueHead| $k:ident) => some { raw := k.raw }
+  | `(polyesqueHead| ($u:term)) => some u
   | _ => none
 
-/--
-The command `name_poly_vars` names variables in
-`MvPolynomial (Fin n) R` for the appropriate value of `n`.
-The notation introduced by this command is local.
+inductive PolyesqueType
+  | polynomial | mvPolynomial | powerSeries | mvPowerSeries | ratFunc | laurentSeries
+deriving DecidableEq, Repr
 
-For `Polynomial (Polynomial (...))`, use the syntax `name_poly_vars R[X][Y][Z]`.
+inductive Mv?
+  | yes | no
+deriving DecidableEq, Repr
 
-Usage:
+def PolyesqueType.mv? : PolyesqueType → Mv?
+  | .mvPolynomial => .yes
+  | .mvPowerSeries => .yes
+  | _ => .no
 
-```lean
-variable (R : Type) [CommRing R]
+def Mv?.Vars : Mv? → Type
+  | .yes => Array Name
+  | .no => Name
 
-name_poly_vars R[X,Y,Z]
+def PolyesqueType.Vars (pt : PolyesqueType) : Type :=
+  pt.mv?.Vars
 
-#check Y -- Y : MvPolynomial (Fin 3) R
-```
--/
-syntax (name := nameMvPolyVars) "name_poly_vars " polyVarsHead "[" ident,+ "]" : command
+def Mv?.Vars.raw : ∀ {b : Mv?} (_ : b.Vars), String
+  | .yes, vars => .intercalate "," (vars.map toString).toList
+  | .no, (var : Name) => s!"{var}"
 
-/--
-The command `name_poly_vars` names variables in `Polynomial (Polynomial (... R))` stacked
-appropriately many times. The notation introduced by this command is local.
+def PolyesqueType.raw : ∀ (pt : PolyesqueType), (_ : pt.Vars) → String
+  | .polynomial, var => s!"[{var.raw}]"
+  | .mvPolynomial, vars => s!"[{vars.raw}]"
+  | .powerSeries, var => s!"[[{var.raw}]]"
+  | .mvPowerSeries, vars => s!"[[{vars.raw}]]"
+  | .ratFunc, var => s!"({var.raw})"
+  | .laurentSeries, var => s!"⸨{var.raw}⸩"
 
-For `MvPolynomial (Fin n) R`, use the syntax `name_poly_vars R[X,Y,Z]`.
+def PolyesqueType.mkC (t : Term) : PolyesqueType → CommandElabM Term
+  | .polynomial => `(Polynomial.C $t)
+  | .mvPolynomial => `(MvPolynomial.C $t)
+  | .powerSeries => `(PowerSeries.C _ $t)
+  | .mvPowerSeries => `(MvPowerSeries.C _ _ $t)
+  | .ratFunc => `(RatFunc.C $t)
+  | .laurentSeries => `(HahnSeries.C $t)
 
-Usage:
+def PolyesqueType.mkX : ∀ (pt : PolyesqueType) (_ : pt.Vars), CommandElabM (Array (Name × Term))
+  | .polynomial, var => return #[(var, ← `(Polynomial.X))]
+  | .mvPolynomial, vars =>
+    vars.zipIdx.mapM fun n ↦ do return (n.fst, ← `(MvPolynomial.X $(quote n.snd)))
+  | .powerSeries, var => return #[(var, ← `(PowerSeries.X))]
+  | .mvPowerSeries, vars =>
+    vars.zipIdx.mapM fun n ↦ do return (n.fst, ← `(MvPowerSeries.X $(quote n.snd)))
+  | .ratFunc, var => return #[(var, ← `(RatFunc.X))]
+  | .laurentSeries, var => return #[(var, ← `(HahnSeries.single 1 1))]
 
-```lean
-variable (R : Type) [CommRing R]
+def PolyesqueType.mkTerm : ∀ pt : PolyesqueType, pt.Vars → Term → CommandElabM Term
+  | .polynomial, _, ih => `(Polynomial $ih)
+  | .mvPolynomial, vars, ih => `(MvPolynomial (Fin $(quote vars.size)) $ih)
+  | .powerSeries, _, ih => `(PowerSeries $ih)
+  | .mvPowerSeries, vars, ih => `(MvPowerSeries (Fin $(quote vars.size)) $ih)
+  | .ratFunc, _, ih => `(RatFunc $ih)
+  | .laurentSeries, _, ih => `(LaurentSeries $ih)
 
-name_poly_vars R[X][Y][Z]
+abbrev Tree := PolyesqueHead × Array ((pt : PolyesqueType) × pt.Vars)
 
-#check Y -- Y : Polynomial (Polynomial (Polynomial R))
-```
--/
-syntax (name := namePolyVars) "name_poly_vars " polyVarsHead "[" sepBy(ident, "][") "]" : command
+-- right = mv
+def parseMv? : TSyntax ``polyesqueMv? → Option (Name ⊕ Array Name)
+  | `(polyesqueMv?| $var,) => pure (Sum.inr #[var.getId])
+  | `(polyesqueMv?| $var:ident) => pure (Sum.inl var.getId)
+  | `(polyesqueMv?| $vars:ident,*) => pure (Sum.inr (vars.getElems.map TSyntax.getId))
+  | _ => .none
 
-@[command_elab nameMvPolyVars, inherit_doc namePolyVars]
-def elabNameMvVariables : CommandElab
-| `(command|name_poly_vars $R:polyVarsHead [$vars:ident,*]) => do
-  let some R := polyVarsHeadToTerm? R | throwUnsupportedSyntax
-  let vars := vars.getElems
-  let size := vars.size
-  let sizeStx : TSyntax `term := quote size
-  for h : idx in [:size] do
-    let var := vars[idx]
-    let var := quote s!"{var.getId}"
-    let idx : TSyntax `term ← `(($(quote idx) : Fin $sizeStx))
-    let cmd ← `(command|local notation3 $var:str =>
-      MvPolynomial.X (R := $R) (σ := Fin $sizeStx) $idx)
-    elabCommand cmd
-| _ => throwUnsupportedSyntax
+def parseBody : TSyntax ``polyesqueBody → Option ((pt : PolyesqueType) × pt.Vars)
+  | `(polyesqueBody| [$vars]) =>
+    match parseMv? vars with
+    | .some (Sum.inl var) => return ⟨.polynomial, var⟩
+    | .some (Sum.inr vars) => return ⟨.mvPolynomial, vars⟩
+    | .none => .none
+  | `(polyesqueBody| [[$vars]]) =>
+    match parseMv? vars with
+    | .some (Sum.inl var) => return ⟨.powerSeries, var⟩
+    | .some (Sum.inr vars) => return ⟨.mvPowerSeries, vars⟩
+    | .none => .none
+  | `(polyesqueBody| ($var)) => return ⟨.ratFunc, var.getId⟩
+  | `(polyesqueBody| ⸨$var⸩) => return ⟨.laurentSeries, var.getId⟩
+  | _ => .none
 
-@[command_elab namePolyVars, inherit_doc namePolyVars]
-def elabNamePolyVariables : CommandElab
-| `(command|name_poly_vars $R:polyVarsHead [$vars:ident][*]) => do
-  let some R := polyVarsHeadToTerm? R | throwUnsupportedSyntax
-  let vars := vars.getElems.reverse
-  let size := vars.size
-  let type : Term ← size.rec (return R) fun _ S ↦ do
-    let S' ← S
-    `(Polynomial $S')
-  let mut term : Term ← `(Polynomial.X)
-  for h : idx in [:size] do
-    let var := vars[idx]
-    let var := quote s!"{var.getId}"
-    let cmd ← `(command|local notation3 $var:str => ($term : $type))
-    elabCommand cmd
-    term ← `(Polynomial.C $term)
-  return ()
-| _ => throwUnsupportedSyntax
+def tree? : Polyesque → Option Tree
+  | `(polyesque| $head:polyesqueHead$body:polyesqueBody*) => do
+    return (head, ← body.mapM parseBody)
+  | _ => .none
 
-end Mathlib.Tactic
+def raw (t : Tree) : String :=
+  t.1.raw.prettyPrint.pretty' ++ .join (t.2.map fun v ↦ v.1.raw v.2).toList
+
+def type (t : Tree) : CommandElabM Term := do
+  let .some head := t.fst.term?
+    | throwError "Unrecognised head"
+  t.snd.foldlM (fun t b ↦ b.1.mkTerm b.2 t) head
+
+end Syntax
+
+section Storage
+
+/-# Storing declared polyesque syntaxes in the environment -/
+
+abbrev Table :=
+  Std.HashMap String Term
+
+/-- An environmental extension to store declared polyesque syntaxes. -/
+abbrev TableExt := SimpleScopedEnvExtension (String × Term) Table
+
+initialize tableExt : TableExt ← registerSimpleScopedEnvExtension <|
+  { addEntry old new := old.modify new.fst fun _ ↦ new.snd
+    initial := {} }
+
+def getTerm (stx : Polyesque) : CoreM Term := do
+  let .some t := tree? stx
+    | throwError m!"Unrecognised syntax: {stx}"
+  let .some t := (tableExt.getState (← getEnv)).get? (raw t)
+    | throwError m!"Polyesque syntax not declared: {stx}"
+  return t
+
+def setTerm (stx : Polyesque) : CoreM Unit := do
+  let .some t := tree? stx
+    | throwError m!"Unrecognised syntax: {stx}"
+  let typ ← liftCommandElabM <| type t
+  trace[name_poly_vars] m!"Setting {raw t} := {typ}"
+  tableExt.add (raw t, typ) .local
+
+@[term_elab polyesque']
+def getTermElab : Term.TermElab := fun stx e ↦ do
+  Term.elabTermEnsuringType (← getTerm ⟨stx⟩) e
+
+end Storage
+
+syntax (name := namePolyVars) "name_poly_vars " polyesque : command
+
+-- syntax (name := nameMvPolyVars) "name_poly_vars " polyVarsHead "[" ident,+ "]" : command
+-- | `(command|name_poly_vars $R:polyVarsHead [$vars:ident,*]) => do
+
+@[command_elab namePolyVars]
+def namePolyVarsElab : CommandElab := fun stx ↦ do
+  let `(command|name_poly_vars $stx:polyesque) := stx
+    | throwError "unrecognised syntax"
+  let .some tree := tree? stx
+    | throwError m!"Failed to parse syntax: {stx}"
+  have str := raw tree
+  let typ ← type tree
+  tableExt.add (str, typ) .local
+  let mut terms : Array (Name × Term) := #[]
+  for ⟨pt, vars⟩ in tree.snd do
+    terms := (← terms.mapM (fun nt ↦ do return (nt.fst, ← pt.mkC nt.snd))) ++ (← pt.mkX vars)
+  unless (terms.map Prod.fst).toList.Nodup do
+    throwError m!"Duplicate variable names found: {terms.map Prod.fst}"
+  for (name, term) in terms do
+    elabCommand <| ← `(command|local notation $(quote s!"{name}"):str => ($term : $typ))
+
+
+-- /--
+-- The command `name_poly_vars` names variables in
+-- `MvPolynomial (Fin n) R` for the appropriate value of `n`.
+-- The notation introduced by this command is local.
+
+-- For `Polynomial (Polynomial (...))`, use the syntax `name_poly_vars R[X][Y][Z]`.
+
+-- Usage:
+
+-- ```lean
+-- variable (R : Type) [CommRing R]
+
+-- name_poly_vars R[X,Y,Z]
+
+-- #check Y -- Y : MvPolynomial (Fin 3) R
+-- ```
+-- -/
+-- syntax (name := nameMvPolyVars) "name_poly_vars " polyVarsHead "[" ident,+ "]" : command
+
+-- /--
+-- The command `name_poly_vars` names variables in `Polynomial (Polynomial (... R))` stacked
+-- appropriately many times. The notation introduced by this command is local.
+
+-- For `MvPolynomial (Fin n) R`, use the syntax `name_poly_vars R[X,Y,Z]`.
+
+-- Usage:
+
+-- ```lean
+-- variable (R : Type) [CommRing R]
+
+-- name_poly_vars R[X][Y][Z]
+
+-- #check Y -- Y : Polynomial (Polynomial (Polynomial R))
+-- ```
+-- -/
+
+-- @[command_elab nameMvPolyVars, inherit_doc namePolyVars]
+-- def elabNameMvVariables : CommandElab
+-- | `(command|name_poly_vars $R:polyVarsHead [$vars:ident,*]) => do
+--   let some R := polyVarsHeadToTerm? R | throwUnsupportedSyntax
+--   let mut RStr : String := R.raw.prettyPrint.pretty'
+--   if R.raw.getId = .anonymous then
+--     RStr := s!"({RStr})"
+--   let vars : Array String := vars.getElems.map (toString ∘ Syntax.getId)
+--   have varsStr : String := ",".intercalate vars.toList
+--   have typeStr : String := s!"{RStr}[{varsStr}]"
+--   let size : ℕ := vars.size
+--   let sizeStx : Term := quote size
+--   let type : Term ← `(MvPolynomial (Fin $sizeStx) $R)
+--   trace[debug] m!"{typeStr}, {type}"
+--   -- e.g. `local notation3 "R[X,Y,Z] => MvPolynomial (Fin 3) R`
+--   elabCommand <| ← `(command|local notation3 $(quote typeStr):str => $type)
+--   for h : idx in [:size] do
+--     let var := vars[idx]
+--     -- e.g. `local notation3 "Y" => (MvPolynomial.X 1 : MvPolynomial (Fin 3) R)`
+--     elabCommand <| ← `(command|local notation3 $(quote var):str =>
+--       (MvPolynomial.X $(quote idx) : $type))
+-- | _ => throwUnsupportedSyntax
+
+-- @[command_elab namePolyVars, inherit_doc namePolyVars]
+-- def elabNamePolyVariables : CommandElab
+-- | `(command|name_poly_vars $R:polyVarsHead [$vars:ident][*]) => do
+--   have typeStr : String := s!"{R}[" ++ "][".intercalate (vars.getElems.map toString).toList ++ "]"
+--   let some R := polyVarsHeadToTerm? R | throwUnsupportedSyntax
+--   have RStr : String := R.raw.prettyPrint.pretty'
+--   let vars : Array String := vars.getElems.map (toString ∘ Syntax.getId)
+--   have varsStr : String := "][".intercalate vars.toList
+--   have typeStr : String := s!"{RStr}[{varsStr}]"
+--   -- we reverse this because the last variable is `Polynomial.X`, the second-to-last variable is
+--   -- `Polynomial.C Polynomial.X`, etc.
+--   let vars := vars.reverse
+--   let size := vars.size
+--   -- build the term `Polynomial (Polynomial (... R))`.
+--   let type : Term ← size.rec (return R) fun _ S ↦ do `(Polynomial $(← S))
+--   -- e.g. `local notation3 "R[X][Y][Z]" => Polynomial (Polynomial (Polynomial R))`
+--   elabCommand <| ← `(command|local notation3 $(quote typeStr):str => $type)
+--   -- build the term `Polynomial.C (Polynomial.C (... Polynomial.X))`.
+--   let mut term : Term ← `(Polynomial.X)
+--   for h : idx in [:size] do
+--     let var := vars[idx]
+--     elabCommand <| ← `(command|local notation3 $(quote var):str => ($term : $type))
+--     term ← `(Polynomial.C $term)
+-- | _ => throwUnsupportedSyntax
+
+
+-- local notation3 "(Fin 37)[d,e]" => Int
+
+-- set_option trace.debug true
+-- name_poly_vars (Fin 37)[d,e]
+-- end Mathlib.Tactic
--- MathlibTest/NamePolyVars.lean
+++ MathlibTest/NamePolyVars.lean
@@ -1,30 +1,20 @@
-import Mathlib.Data.ZMod.Defs
+import Mathlib.Algebra.Polynomial.Basic
+import Mathlib.Algebra.MvPolynomial.Basic
+import Mathlib.RingTheory.PowerSeries.Basic
+import Mathlib.RingTheory.MvPowerSeries.Basic
+import Mathlib.FieldTheory.RatFunc.AsPolynomial
+import Mathlib.RingTheory.LaurentSeries
 import Mathlib.Tactic.Ring.NamePolyVars
 
 variable (R : Type) [CommRing R]
 
-name_poly_vars R[X,Y,Z]
-name_poly_vars Int[q]
-name_poly_vars R[S][T][U]
-name_poly_vars (ZMod 37)[d,e]
+-- name_poly_vars R[X,Y,Z][t][[a,b,c]][[d]](u)⸨v⸩[p,][[q,]]
+-- set_option quotPrecheck false
+-- name_poly_vars R[x]
+-- #check x
 
-noncomputable example : Vector (MvPolynomial (Fin 3) R) 3 :=
-  have : X = MvPolynomial.X 0 := rfl
-  have : Y = MvPolynomial.X 1 := rfl
-  have : Z = MvPolynomial.X 2 := rfl
-  #v[X, Y, Z]
-
-noncomputable example : Polynomial ℤ :=
-  have : q = Polynomial.X := rfl
-  q
-
-noncomputable example : Vector (Polynomial (Polynomial (Polynomial R))) 3 :=
-  have : S = Polynomial.C (Polynomial.C Polynomial.X) := rfl
-  have : T = Polynomial.C Polynomial.X := rfl
-  have : U = Polynomial.X := rfl
-  #v[S, T, U]
-
-noncomputable example : Vector (MvPolynomial (Fin 2) (ZMod 37)) 2 :=
-  have : d = MvPolynomial.X 0 := rfl
-  have : e = MvPolynomial.X 1 := rfl
-  #v[d, e]
+elab "asdf" : term => do
+  have bar : Lean.Ident := Lean.mkIdent `foo
+  Lean.Elab.Term.elabTermEnsuringType (← `($bar)) .none
+axiom foo : Type
+#check asdf
